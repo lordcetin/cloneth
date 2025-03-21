7786b2e53e2444546c9dc7d9f99389ed9d5c4b05
Yunus Çetin <93588469+lordcetin@users.noreply.github.com>
2025-03-21 16:27:48 +0300

    sfhgsfah

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
+  skipWaiting: true,
   scope: '/',
   sw: 'sw.js',
   // dynamicStartUrl: false,
